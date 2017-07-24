a0858662ef3d28c44d31eefe3910bfda7a88a18c
vagrant <vagrant@homestead>
2017-07-24 04:57:00 +0000

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(User $user)
+    { 
+        return view('user/index', ['users' => $user::get()]); 
+    }
+}
